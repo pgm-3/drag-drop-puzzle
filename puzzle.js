307d5283a34182a37ab3a86459f1f03c19aba7e8
timdpaep <tim.depaepe@gmail.com>
2021-02-14 12:05:54 +0100

    added solution

--- puzzle.js
+++ puzzle.js
@@ -18,17 +18,47 @@ const getRandomPictureArray = () => {
  * Cancellation event
  */
 
-// TODO: Create a cancel method that will prevent default behaviour
+const cancel = (e) => {
+  if(e.preventDefault) e.preventDefault();
+  if(e.stopPropagation) e.stopPropagation();
+  return false;
+}
 
 /**
  * Create the Drag & Drop API Event Callbacks
  */
 
-// TODO: Add a dragStart function
+const dragStart = (e) => {
+  e.dataTransfer.setData('text/plain', e.target.dataset.dropZone);
+}
 
-// TODO: Add a dragOver function
+const dragOver = (e) => {
+  cancel(e); // cancels default behaviour
+}
 
-// TODO: Add a drop function
+const drop = (e) => {
+  cancel(e); // cancels default behaviour
+
+  // drop in a target that IS in fact a dropzone
+  if (e.target.className === 'drag-element') {
+    // Get the Id of the zone we are coming from
+    const comingFromZoneId = e.dataTransfer.getData('Text')
+
+    // Coming From
+    const comingFromZone = document.getElementById(comingFromZoneId);
+    const comingFromImage = (comingFromZone.getElementsByClassName('drag-element'))[0];
+
+    // Dropped On
+    const droppedOnZone = document.getElementById(e.target.dataset.dropZone);
+    const droppedOnImage = (droppedOnZone.getElementsByClassName('drag-element'))[0];
+
+    // Switch Images
+    comingFromZone.appendChild(droppedOnImage);
+    comingFromImage.dataset.dropZone = droppedOnImage.dataset.dropZone;
+    droppedOnZone.appendChild(comingFromImage);
+    droppedOnImage.dataset.dropZone = comingFromZoneId;
+  }
+}
 
 const initPuzzle = () => {
   // get a random picture array
@@ -45,14 +75,21 @@ const initPuzzle = () => {
     imgElement.classList.add('drag-element');
     imgElement.dataset.dropZone = `dropZone${i+1}`;
 
-    // TODO: add the dragstart event
+    // TODO: add some drag/drop events
+    imgElement.addEventListener('dragstart', dragStart);
 
     // add to dropzone
     const dropZone = document.getElementById(`dropZone${i+1}`);
     dropZone.appendChild(imgElement);
   }
 
-  // TODO init the dropZones and add dragover & drop event
+  // TODO init the dropZones
+  const dropZones = document.querySelectorAll('.drop-zone');
+  dropZones.forEach((dropZone) => {
+    //dropZone.addEventListener('dragstart', dragStart);
+    dropZone.addEventListener('dragover', dragOver);
+    dropZone.addEventListener('drop', drop);
+  });
 }
 
 initPuzzle();
